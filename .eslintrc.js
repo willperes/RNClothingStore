ed8207b711f44a88fb22e3c750a7119e5f11ff45
willperes <thewidocs@gmail.com>
2024-08-30 23:11:42 -0300

    feat: project linting and import patterns config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,40 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'import/order': [
+          'error',
+          {
+            groups: ['external', 'builtin', 'internal', 'parent', 'sibling'],
+            pathGroups: [
+              {
+                pattern: 'react+(|-native)',
+                group: 'external',
+                position: 'before',
+              },
+              {
+                pattern: '@/+(components|domain|features)',
+                group: 'internal',
+                position: 'before',
+              },
+              {
+                pattern: './',
+                group: 'internal',
+                position: 'before',
+              },
+            ],
+            pathGroupsExcludedImportTypes: ['react+(|-native)'],
+            alphabetize: {
+              order: 'asc',
+              caseInsensitive: true,
+            },
+            'newlines-between': 'always',
+          },
+        ],
+      },
+    },
+  ],
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'module-resolver',
+    {
+      root: '.',
+      alias: {
+        '@/components': './src/components',
+      },
+    },
+  ],
 };
